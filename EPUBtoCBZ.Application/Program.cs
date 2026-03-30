bb4dacf76f6515ba1628b4db9f5ff3e1c59b4b3f
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2026-03-29 20:59:25 -0400

    Project Initialization

---
+++ EPUBtoCBZ.Application/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EPUBtoCBZ.Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
